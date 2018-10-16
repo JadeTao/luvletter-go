2121a10caef8fe9862cb306ec10fd0c4fe2b85a7
jader_tao <jader_tao@qq.com>
2018-10-16 11:06:23 +0800

    dev(router): add prefix

--- router/router.go
+++ router/router.go
@@ -10,16 +10,28 @@ import (
 	"luvletter/app/user"
 )
 
-// GETRouters RouterConfig for GET.
-var GETRouters = map[string]echo.HandlerFunc{
-	"/letter": letter.GetAll,
+// Prefix ...
+var Prefix = "/api/v1"
+
+// PrefixMapper ...
+func PrefixMapper(router map[string]echo.HandlerFunc, prefix string) map[string]echo.HandlerFunc {
+	var withPrefixRouter = make(map[string]echo.HandlerFunc)
+	for key, value := range router {
+		withPrefixRouter[prefix+key] = value
+	}
+	return withPrefixRouter
 }
 
+// GETRouters RouterConfig for GET.
+var GETRouters = PrefixMapper(map[string]echo.HandlerFunc{
+	"/letter": letter.GetAll,
+}, Prefix)
+
 // POSTRouters RouterConfig for POST.
-var POSTRouters = map[string]echo.HandlerFunc{
+var POSTRouters = PrefixMapper(map[string]echo.HandlerFunc{
 	"/login":    user.Login,
 	"/register": user.Register,
 	"/letter":   letter.Save,
 	"/tag":      tag.Save,
 	"/mood":     mood.Save,
-}
+}, Prefix)
--- router/skip.go
+++ router/skip.go
@@ -6,12 +6,20 @@ import (
 
 // Skip ...
 func Skip(c echo.Context) bool {
-	skippedPath := map[string]bool{
+	skippedPath := prefixSkipMapper(map[string]bool{
 		"/login":    true,
 		"/register": true,
-	}
+	}, Prefix)
 	if skippedPath[c.Path()] {
 		return true
 	}
 	return false
+}
+
+func prefixSkipMapper(router map[string]bool, prefix string) map[string]bool {
+	var withPrefixRouter = make(map[string]bool)
+	for key, value := range router {
+		withPrefixRouter[prefix+key] = value
+	}
+	return withPrefixRouter
 }
